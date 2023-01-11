9ecbbbdf5b48b67259c3921d3f5a65132b4acb75
JoleikNord <94911770+JoleikNord@users.noreply.github.com>
2023-01-11 11:27:08 +0000

    commit

--- src/IonSimulation.jl
+++ src/IonSimulation.jl
@@ -6,6 +6,8 @@ import FFTW
 using Plots
 import Statistics
 import LinearAlgebra: mul!, ldiv!, inv
+using HDF5
+using Dates
 ################# Creating a grid modeled after the Luna free grid module ######################
 abstract type SpacetimeGrid end
 struct Grid <: SpacetimeGrid
@@ -66,7 +68,6 @@ function FreeGrid(Rx, Nx, Ry, Ny, δt, Nt; window_factor=0.1)
 end
 
 Grid(R, N, δT, NT) = FreeGrid(R, N, R, N, δT, NT)
-grid = Grid(320e-6, 64, 50e-18, 1024)
 
 ######################## Creating gausbeam #################################
 
@@ -88,12 +89,11 @@ function Efoc(Grid::SpacetimeGrid, w0, λ0, P, fwhm, τ, θ)
     ω0 = PhysData.wlfreq(λ0)
     I = 2 * P / (π * w0^2)
     A0 = Tools.intensity_to_field(I) 
-    degtorad = θ * pi/180
     Efield = zeros(ComplexF64, (size(Grid.t)[1], size(Grid.x)[1], size(Grid.y)[1]))
     for (xidx, xi) in enumerate(Grid.x)
         for (yidx, yi) in enumerate(Grid.y)
             for (tidx, ti) in enumerate(Grid.t)
-                gausbeam =  exp(-(xi^2 + yi^2) / w0^2) * exp(1im * sin(degtorad) * xi * (ω0 / PhysData.c))
+                gausbeam =  exp(-(xi^2 + yi^2) / w0^2) * exp(1im * sin(θ * pi/180) * xi * (ω0 / PhysData.c))
                 gauspulse =  Maths.gauss(ti.-τ; fwhm = fwhm) * exp(1im * ω0 * (ti.-τ))
                 Efield[tidx, xidx, yidx] = @. A0 * gauspulse * gausbeam 
             end
@@ -128,7 +128,7 @@ by a distance z. (e.g If the propagator object is called p, p(E, z))
 - `Electric field::Array{Complex64, 3}` : Electric field in space and time created by the "Efoc" function
 - `w0::Real` : Beam waste in focus
 - `λ0::Real` : Central wavelength
-- `flags::???` : FFTW plan for the electric field array.
+- `flags::FFTW_plan` : FFTW plan for the electric field array.
 """
 function Propagator(Grid::SpacetimeGrid, E, w0, λ0; flags=FFTW.MEASURE)
     FT = FFTW.plan_fft(E; flags)
@@ -152,7 +152,7 @@ Calculates the fraction of ionisation induced by an electric field.
 
 # Arguments
 - `Grid::SpacetimeGrid` : Grid that defines the time and space of interest
-- `rate::???` : Ionisation rate calulated with the LUNA/Ionisation ionrate_fun function. 
+- `rate::ionrate` : Ionisation rate calulated with the LUNA/Ionisation ionrate_fun function. 
                 (e.g ppt = Ionisation.ionrate_fun!_PPTcached(:He, λ0))
 - `Electric field::Array{Complex64, 3 or 4}` : Electric field in space and time created by the "Efoc" function.
                                                also works for propagation cubes.
@@ -185,7 +185,7 @@ end
 Creates a mask that can be applyed to block part of a beam.
 
 # Arguments
-- `Grid::SpacetimeGrid` : Grid that defines the time and space of interest
+- `Grid::SpacetimeGrid` : Grid that defines the time and space of interest.
 - `OuterRadius::Float64` : Outer radius of the mask in m. 
 - `InnerRadius::Float64` : Inner radius of the mask in m.
 """
@@ -250,14 +250,14 @@ function PropCube(p::Propagator, E, zrange::Tuple{Float64, Float64}, Nz::Int64)
     PropCube;
 end
 ## Plotting and animation functions --------------------------------------------
-function PlotPulse(Grid::SpacetimeGrid, E)
+function PlotPulse(Grid::SpacetimeGrid, E) 
     plt.figure()
     plt.pcolormesh(Grid.x, Grid.y, abs2.(E[1, :, :]))
     plt.colorbar()
     plt.gcf()
 end
 
-function Animate(Grid::SpacetimeGrid, Ecube, fps, fname::String)
+ function Animate(Grid::SpacetimeGrid, Ecube, fps, fname::String)
     frames = size(Ecube, 1)
     anim_dummy = @animate for i ∈ 1:frames
        #fig = plot(y * 1e6, abs2.(PulseCube[i, Nt÷2, Nx÷2+1, :]),c = :viridis, ylims = (0, 1e20))
@@ -266,69 +266,89 @@ function Animate(Grid::SpacetimeGrid, Ecube, fps, fname::String)
    end
    gif(anim_dummy, fname * ".gif", fps = fps)
 end
-#Currently under construction
-#=
-mutable struct Scan{nT}
+##Currently under construction
+
+ mutable struct Scan{nT}
     λ::Float64
     PeakP:: Float64
     fwhm::Float64
     w0::Float64
     f::Float64
     θ::Float64
-    τ::Float64
     Grid::SpacetimeGrid
     Mask::Array{ComplexF64, 2}
     E::Array{nT, 3}
     p::Propagator
     rate::Any
-    folname::String
-    fname::String
 end
+"""
+    CreateScan(λ0, PeakP,  fwhm, w0, f, θ, Grid, Mask, p::Propagator, rate)
 
+Constructs a `Scan` for running autocorrelation delay scans using `Scan`(...)
 
-function CreateScan(λ0::Float64, PeakP:: Float64,  fwhm::Float64, w0::Float64, f::Float64, θ::Float64, τ::Float64, Grid::SpacetimeGrid, Mask::Array{ComplexF64, 2},p::Propagator,rate,  folname::String,fname::String)
-    E = Efoc(Grid, w0, λ0, PeakP, fwhm, τ, θ)
+# Arguments
+- `λ0::Float64` : Central wavelength
+- `PeaP::Float64` : Peak power of the pulse.
+- `fwhm::Float64` : Full-width-half-maximum of the pulse.
+- `w0::Float64` : Beam waist.
+- `f::Float64` : f-number of the focusing optics.
+- `Θ::Float64` : Angle under which the beam propagates.
+- `Grid::SpacetimeGrid` : Grid that defines the time and space of interest
+- `Mask::Array{size Grid[x,y]}` : Mask that will be applied to the beam at position -f.
+                                  The mask should have the same size has the x,y space in the Grid. The mask will simply be
+                                  multiplied to the signal.
+- `Propagator p::Propagator` : The Propagator that will be used to propagate the electric field E. 
+                               To create a Propagator use the "Propagator" function. 
+- `rate::Ionrate` : Ionisation rate calulated with the LUNA/Ionisation ionrate_fun function. 
+                    (e.g ppt = Ionisation.ionrate_fun!_PPTcached(:He, λ0))
+
+"""
+ function CreateScan(λ0::Float64, PeakP:: Float64,  fwhm::Float64, w0::Float64, f::Float64, 
+    θ::Float64, Grid::SpacetimeGrid, Mask::Array{ComplexF64, 2}, 
+    p::Propagator, rate)
+    E = Efoc(Grid, w0, λ0, PeakP, fwhm, θ)
     Emask = ApplyMask(Grid, E, Mask, f, p)
-    Scan(λ0, PeakP,  fwhm, w0, f, θ, τ, Grid, Mask, Emask, p, rate, folname,fname)
+    Scan(λ0, PeakP,  fwhm, w0, f, θ, Grid, Mask, Emask, p, rate)
 end
-function (dscan::Scan)(DRange::Tuple{Float64, Float64}, dsteps::Int64, dmask::Array{ComplexF64, 2})
-    IonMap = []
+
+function (dscan::Scan)(DRange::Tuple{Float64, Float64}, dsteps::Int64, dmask::Array{ComplexF64, 2}, fpath, fname, )
     E = copy(dscan.E)
     start, stop = DRange
     delay = collect(range(start, stop, dsteps))
+    IonMap = zeros(length(delay))
+    k = 1
     for i ∈ delay
         dscan.E = Efoc(dscan.Grid, dscan.w0, dscan.λ, dscan.PeakP, dscan.fwhm, i, dscan.θ)
         dscan.E = ApplyMask(dscan.Grid, dscan.E, dmask, dscan.f, dscan.p)
         dscan.E += E
-        push!(IonMap, mean(Ionfrac(Gird, dscan.rate, dscan.E)))
-        printnl("Step $i from $delay")
+        IonMap[k] = Statistics.mean(Ionfrac(dscan.Grid, dscan.rate, dscan.E))
+        #push!(IonMap, Statistics.mean(Ionfrac(dscan.Grid, dscan.rate, dscan.E)))
+        println("Step $k from $dsteps")
+        k += 1
     end
-    IonMap
-end
-##
-
-function DelayScan(Grid::SpacetimeGrid, w0, λ0, P, fwhm, Ionrate, delrange::Tuple, delsteps::Int64, Mask, Maskdel, f, Prop, θ, θsteps = 0, sfol = " ")
-    delay = collect(range(delrange[1], delrange[2], delsteps))
-    if θ isa(Tuple)
-        angles = collect(range(θ[1], θ[2], θsteps))
-        for j in angles
-            k = 0
-            Delayscanner.E = Efoc(Grid, w0, λ0, P, fwhm, j)
-            Delayscanner.E = ApplyMask(Grid, Delayscanner.Ek, Mask, f, Prop)
-            for i ∈ delay
-                Delayscanner.Ed = Efoc(Grid, w0, λ0, P, fwhm, i, -j)
-                Delayscanner.Ed = ApplyMask(Grid, Delayscanner.Ed, Maskl, f, p)
-                Delayscanner.Ed = Delayscanner.E + Delayscanner.Ed
-                Iondelay = Ionfrac(Grid, Ionrate, Delayscanner.Ed)
-                push!(Delayscanner.IonMap, mean(Delayscanner.Iondelay))
-                k += 1
-                println("Step $k of $delaysteps")
+    date = Dates.format(now(), "yyyy-mm-dd")
+    folpath = mkpath(joinpath(fpath, date))
+    filename = fname*"_"* Dates.format(now(), "HH-MM-SS") * ".h5"
+    filepath = joinpath(folpath, filename) 
+    if !isfile(filepath)
+        HDF5.h5open(filepath, "w") do file
+            create_group(file, "data")
+            create_group(file, "params")
+            f = file["data"]
+            g = file["params"]
+            #HDF5.create_dataset(f, "Ionisation_Fraction", Float64, 2)
+            f["Ionisation_Fraction"] = IonMap
+            f["delay"] = delay 
+            parnames = ["λ", "PeakP", "fwhm", "w0", "f", "θ", "τ"]
+            parvalues = [dscan.λ, dscan.PeakP, dscan.fwhm, dscan.w0, dscan.f, dscan.θ, dscan.τ]
+            for (key, values) in zip(parnames, parvalues)
+                g[key] = values 
             end
-            @save "IonMap_dshape_angle=$j.jld2" delay IonMap
+            
+        end
+    end
 
-            plt.plot(delay, Delayscanner.IonMap, label = "Crossing angle = ${round(j, digits = 2)}")
-            plt.legend()
-            plt.gcf()
 end
-=#
+end
+
 
--- test/runtests.jl
+++
@@ -1,6 +0,0 @@
-using IonSimulation
-using Test
-
-@testset "IonSimulation.jl" begin
-    # Write your tests here.
-end
